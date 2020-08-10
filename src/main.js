40e32e465a68a5da950a8b3045dc801a934092c8
Raphi <raphael.antonietti@liip.ch>
2020-08-10 18:25:42 +0200

    Implement in headings

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./assets/css/main.postcss";
+import CodeLoader from "@/components/CodeLoader.vue";
 import "./plugins/axe.js";
 import "prismjs";
 import "prismjs/themes/prism.css";
 Vue.config.productionTip = false;
+Vue.component("code-loader", CodeLoader);
 
 new Vue({
   router,
--- src/views/components/Headings.vue
+++ src/views/components/Headings.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="space-y-6">
-    <h1>Heading 1</h1>
-    <h2>Heading 2</h2>
-    <h3>Heading 3</h3>
-    <h4>Heading 4</h4>
+    <h1 class="reference--heading-1">Heading 1</h1>
+    <code-loader reference=".reference--heading-1" lang="html" />
+    <h2 class="reference--heading-2">Heading 2</h2>
+    <code-loader reference=".reference--heading-2" lang="html" />
+    <h3 class="reference--heading-2">Heading 3</h3>
+    <code-loader reference=".reference--heading-3" lang="html" />
+    <h4 class="reference--heading-3">Heading 4</h4>
+    <code-loader reference=".reference--heading-4" lang="html" />
   </div>
 </template>
