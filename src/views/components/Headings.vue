86b13c99276e66035cd98e96fe91552ef06cb22c
Sylvain Aerni <sylvain.aerni@gmail.com>
2020-08-11 20:08:52 +0200

    write content for typography

--- src/views/components/Headings.vue
+++ src/views/components/Headings.vue
@@ -1,15 +1,21 @@
 <template>
-  <div class="space-y-6">
+  <div class="space-y-6 max-w-4xl">
+    <h1>Typography</h1>
+    <p>
+      Describe here the importance of typography in the swiss design history.
+      Talk about the Font choice, Frutiger, and give some words about Adrian the Greatest. Rythme, precision, vertical spacings could be evoked too.
+    </p>
+    <h2>Headings</h2>
+    <p>
+      Headings serve to structure the content of a page. The higher the heading is in the structural hierarchy (e.g. H1), the more important it is, and the more weight is given to it by search engines such as Google. Generally speaking, not too many hierarchical levels should be used, as this has a negative impact on the clarity of the structure. Heading levels H1 through H4 are therefore clearly differentiated in their formatting (while levels H5 and H6 are merely defined pro forma).</p>
+    <h5>Accessibility</h5>
+    <p>
+      To conform accessibiliity requirement titles have to respect a sequence from 1 to 6
+    </p>
     <code-loader lang="html">
       <h1>Heading 1</h1>
-    </code-loader>
-    <code-loader lang="html">
       <h2>Heading 2</h2>
-    </code-loader>
-    <code-loader lang="html">
       <h3>Heading 3</h3>
-    </code-loader>
-    <code-loader lang="html">
       <h4>Heading 4</h4>
     </code-loader>
   </div>
