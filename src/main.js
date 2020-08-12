bc40f6270806724723b495ed6d9099ac09ee10cb
Raphi <raphael.antonietti@liip.ch>
2020-08-12 18:21:58 +0200

    Add icon loading

--- src/main.js
+++ src/main.js
@@ -7,10 +7,12 @@ import CodeLoader from "@/components/CodeLoader.vue";
 import "./plugins/axe.js";
 import Prism from "vue-prism-component";
 import "prismjs";
+import SVGIcon from "@/components/SVGIcon";
 import "prismjs/themes/prism.css";
 Vue.config.productionTip = false;
 Vue.component("code-loader", CodeLoader);
 Vue.component("prism", Prism);
+Vue.component("svg-icon", SVGIcon);
 
 new Vue({
   router,
