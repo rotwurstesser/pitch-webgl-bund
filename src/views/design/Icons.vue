ab86725bd19d2c6437083e51409c411d7a3363e0
Raphi <raphael.antonietti@liip.ch>
2020-08-13 19:15:24 +0200

    Add Icons

--- src/views/design/Icons.vue
+++ src/views/design/Icons.vue
@@ -2,14 +2,39 @@
   <div class="space-y-6 max-w-4xl">
     <h1>Icons</h1>
     <p>
-      to use this Element click on the
+      Several icons are availble in the Design System this Set will be
+      continually expanded upon.
+    </p>
+    <p>
+      to use an Element click on the
       <svg-icon class="mx-2 stroke-2 inline-block" name="figma" /> icon
     </p>
+    <h2>Swiss Icons</h2>
+    <p>these icons represent our love to switzerland</p>
     <iframe
       style="border: 1px solid rgba(0, 0, 0, 0.1);"
       width="800"
       height="450"
       src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FtohWKnhnuXCVoG1o1br2MK%2FBund-Design-System%3Fnode-id%3D130%253A3984&chrome=DOCUMENTATION"
+      allowfullscreen
+    ></iframe>
+    <h2>Action Icons</h2>
+    <p>Action icons are used to signal and describe interaction.</p>
+    <iframe
+      style="border: 1px solid rgba(0, 0, 0, 0.1);"
+      width="800"
+      height="450"
+      src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FtohWKnhnuXCVoG1o1br2MK%2FBund-Design-System%3Fnode-id%3D191%253A4336&chrome=DOCUMENTATION"
+      allowfullscreen
+    ></iframe>
+
+    <h2>Navigation Icons</h2>
+    <p>Navigation icons are used as part of the UI.</p>
+    <iframe
+      style="border: 1px solid rgba(0, 0, 0, 0.1);"
+      width="800"
+      height="450"
+      src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FtohWKnhnuXCVoG1o1br2MK%2FBund-Design-System%3Fnode-id%3D191%253A4737&chrome=DOCUMENTATION"
       allowfullscreen
     ></iframe>
   </div>
