46b47bc751ca50954f0e2174eafd890dc95ce8a7
Marton Orehovszky <marton.orehovszky@liip.ch>
2020-08-13 17:23:19 +0200

    feat(designers-figma-iframe): fix linting issues

--- src/views/menus/GetStartedMenu.vue
+++ src/views/menus/GetStartedMenu.vue
@@ -69,33 +69,21 @@
       >
         Other stuff
       </h3>
-      <div
-        class="mt-1 space-y-1"
-        role="group"
-        aria-labelledby="teams-headline"
-      >
+      <div class="mt-1 space-y-1" role="group" aria-labelledby="teams-headline">
         <a
           href="#"
           class="no-underline group flex items-center px-3 py-2 leading-5 text-gray-700 rounded-md hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition ease-in-out duration-150"
         >
-          <span
-            class="w-2.5 h-2.5 mr-4 bg-indigo-500 rounded-full"
-          ></span>
-          <span class="truncate">
-            Stuff A
-          </span>
+          <span class="w-2.5 h-2.5 mr-4 bg-indigo-500 rounded-full"></span>
+          <span class="truncate">Stuff A</span>
         </a>
 
         <a
           href="#"
           class="no-underline group flex items-center px-3 py-2 leading-5 text-gray-700 rounded-md hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition ease-in-out duration-150"
         >
-          <span
-            class="w-2.5 h-2.5 mr-4 bg-teal-400 rounded-full"
-          ></span>
-          <span class="truncate">
-            Stuff 2
-          </span>
+          <span class="w-2.5 h-2.5 mr-4 bg-teal-400 rounded-full"></span>
+          <span class="truncate">Stuff 2</span>
         </a>
       </div>
     </div>
