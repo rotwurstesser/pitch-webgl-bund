30a61fa6dcabc61168fbf6bf94a06e5754c21c47
Raphi <raphael.antonietti@liip.ch>
2020-08-13 18:25:38 +0200

    Add figma explanation

--- src/views/Designers.vue
+++ src/views/Designers.vue
@@ -5,5 +5,10 @@
       In order to get started with our Design System, make sure you have a Figma
       Account. In the Sidebar you can find all our Elements ready to use.
     </h2>
+    <p>
+      If you see any Elements you would like to use in your App or if you want
+      to play around with them click on the figma icon
+      <svg-icon class="ml-2 stroke-2" name="figma" />
+    </p>
   </div>
 </template>
