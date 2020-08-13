576d6b440eba69d51b3192a58dc2aaa2a960bd5e
Marton Orehovszky <marton.orehovszky@liip.ch>
2020-08-13 17:23:02 +0200

    feat(designers-figma-iframe): add iframes to page

--- src/views/Designers.vue
+++ src/views/Designers.vue
@@ -1,5 +1,25 @@
 <template>
   <div>
-    <h1>Designers</h1>
+    <h1 class="mb-16">Designers</h1>
+    <h2 class="mb-12">Icons</h2>
+    <div class="m-6 mb-12">
+      <iframe
+        style="border: 1px solid rgba(0, 0, 0, 0.1);"
+        width="800"
+        height="450"
+        src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FtohWKnhnuXCVoG1o1br2MK%2FBund-Design-System%3Fnode-id%3D116%253A3983&chrome=DOCUMENTATION"
+        allowfullscreen
+      ></iframe>
+    </div>
+    <h2 class="mb-12">Buttons</h2>
+    <div class="m-6 mb-12">
+      <iframe
+        style="border: 1px solid rgba(0, 0, 0, 0.1);"
+        width="800"
+        height="450"
+        src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FtohWKnhnuXCVoG1o1br2MK%2FBund-Design-System%3Fnode-id=116%3A3991&chrome=DOCUMENTATION"
+        allowfullscreen
+      ></iframe>
+    </div>
   </div>
 </template>
