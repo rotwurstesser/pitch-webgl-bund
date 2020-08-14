f8b975b199d5d1831f9d164ed26034af9c58410d
Sylvain Aerni <sylvain.aerni@gmail.com>
2020-08-14 11:04:28 +0200

    remove unused page

--- src/views/GetStarted.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="lg:w-2/3 space-y-6">
-    <h1 class="font-bold mb-16">Get Started</h1>
-    <h2>
-      [Get Started introduction, general information about the guidelines and
-      the components, how the design system is structured]
-    </h2>
-  </div>
-</template>
