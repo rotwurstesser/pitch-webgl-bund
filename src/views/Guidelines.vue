4f0c51722497fb68937f29a158c4890b0a0d8241
Raphi <raphael.antonietti@liip.ch>
2020-08-14 19:31:49 +0200

    Update docs

--- src/views/Guidelines.vue
+++ src/views/Guidelines.vue
@@ -16,6 +16,29 @@
       Wordmark needs to contain every Language as to reflect Switzerlands
       Diversity.
     </p>
+    <svg-icon name="logo" class="h-24 my-4 w-auto fill-current" />
+    <h2 class="mt-4">Key Principles</h2>
+    <p class="mb-4">
+      The Design System aims to align with the Guidelines of the Swiss
+      Confederation and therefore our principles are:
+    </p>
+    <ul>
+      <li class="flex space-x-4">
+        <svg-icon class="h-6 w-12" name="arrow" /> diverse
+      </li>
+      <li class="flex space-x-4">
+        <svg-icon class="h-6 w-12" name="arrow" /> flexible
+      </li>
+      <li class="flex space-x-4">
+        <svg-icon class="h-6 w-12" name="arrow" /> conversational
+      </li>
+      <li class="flex space-x-4">
+        <svg-icon class="h-6 w-12" name="arrow" /> connected
+      </li>
+      <li class="flex space-x-4">
+        <svg-icon class="h-6 w-12" name="arrow" /> practical
+      </li>
+    </ul>
   </div>
 </template>
 <script>
