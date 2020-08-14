2e8474a739fd4f692514901506485cb9221ca599
Raphi <raphael.antonietti@liip.ch>
2020-08-14 22:53:18 +0200

    Add app example

--- src/views/Examples.vue
+++ src/views/Examples.vue
@@ -2,9 +2,28 @@
   <div>
     <h1 class="font-bold mb-16">Examples</h1>
     <h2 class="my-4">Newspage</h2>
-    <img class="max-w-screen-sm" src="@/assets/newspage.png" />
+    <div class="p-8 bg-gray-200 flex justify-center">
+      <img class="max-w-screen-sm" src="@/assets/newspage.png" />
+    </div>
     <a
       href="https://www.figma.com/file/tohWKnhnuXCVoG1o1br2MK/Bund-Design-System?node-id=403%3A10"
+      target="_blank"
+      class="border-0 border-gray-400 border-r btn btn--tertiary btn--xs cursor-pointer inline-block"
+    >
+      Open in Figma
+    </a>
+    <span
+      alt="This has not been implemented in the prototype"
+      class="border-0 btn btn--tertiary btn--xs inline-block text-gray-400 hover:text-gray-400 cursor-not-allowed"
+    >
+      See the coded page
+    </span>
+    <h2 class="mt-16 mb-4">Appdesign - Swisscovid</h2>
+    <div class="p-8 bg-gray-200 flex justify-center">
+      <img class="max-w-xs" src="@/assets/appexample.png" />
+    </div>
+    <a
+      href="https://www.figma.com/file/tohWKnhnuXCVoG1o1br2MK/Bund-Design-System?node-id=403%3A233"
       target="_blank"
       class="border-0 border-gray-400 border-r btn btn--tertiary btn--xs cursor-pointer inline-block"
     >
