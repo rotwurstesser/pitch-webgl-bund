8132a6c4d0e4d68181a888033573d49c21c7509c
Raphi <raphael.antonietti@liip.ch>
2020-08-14 21:36:50 +0200

    add example

--- src/views/Examples.vue
+++ src/views/Examples.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
     <h1 class="font-bold mb-16">Examples</h1>
+    <h2 class="my-4">Newspage</h2>
+    <img class="max-w-screen-sm" src="@/assets/newspage.png" />
+    <a
+      href="https://www.figma.com/file/tohWKnhnuXCVoG1o1br2MK/Bund-Design-System?node-id=403%3A10"
+      target="_blank"
+      class="border-0 border-gray-400 border-r btn btn--tertiary btn--xs cursor-pointer inline-block"
+    >
+      Open in Figma
+    </a>
+    <span
+      alt="This has not been implemented in the prototype"
+      class="border-0 btn btn--tertiary btn--xs inline-block text-gray-400 hover:text-gray-400 cursor-not-allowed"
+    >
+      See the coded page
+    </span>
   </div>
 </template>
