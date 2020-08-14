484de469926be78a827b9fb81fbd6d8d89c7401b
Raphi <raphael.antonietti@liip.ch>
2020-08-14 19:37:04 +0200

    add navi text

--- src/views/Components.vue
+++ src/views/Components.vue
@@ -6,7 +6,9 @@
       component has been designed and coded to solve a specific UI problem, such
       as presenting a list of options, enabling submission of a form, providing
       feedback to the user, and so on. All of the components in have been
-      designed to work harmoniously together, as parts of a greater whole.
+      designed to work harmoniously together, as parts of a greater whole. The
+      Elements in the system will continually expanded upon. Use the navigation
+      to your right to navigate through them.
     </p>
   </div>
 </template>
