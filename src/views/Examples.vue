a6bcc135c8d1b218f5a211d9acf738b1816e30a5
Raphi <raphael.antonietti@liip.ch>
2020-08-14 23:33:38 +0200

    Add lovelife

--- src/views/Examples.vue
+++ src/views/Examples.vue
@@ -35,5 +35,22 @@
     >
       See the coded page
     </span>
+    <h2 class="mt-16 mb-4">Appdesign - Swisscovid</h2>
+    <div class="p-8 bg-gray-200 flex justify-center">
+      <img class="max-w-screen-sm" src="@/assets/lovelife.png" />
+    </div>
+    <a
+      href="https://www.figma.com/file/tohWKnhnuXCVoG1o1br2MK/Bund-Design-System?node-id=411%3A0"
+      target="_blank"
+      class="border-0 border-gray-400 border-r btn btn--tertiary btn--xs cursor-pointer inline-block"
+    >
+      Open in Figma
+    </a>
+    <span
+      alt="This has not been implemented in the prototype"
+      class="border-0 btn btn--tertiary btn--xs inline-block text-gray-400 hover:text-gray-400 cursor-not-allowed"
+    >
+      See the coded page
+    </span>
   </div>
 </template>
