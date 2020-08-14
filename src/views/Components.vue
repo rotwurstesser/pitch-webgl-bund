0c721f5987d0a9dda0fe4b23819558d57ac368cc
Raphi <raphael.antonietti@liip.ch>
2020-08-14 19:34:57 +0200

    Add best components text evaaaaaaaaaaa thanks carbon

--- src/views/Components.vue
+++ src/views/Components.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
     <h1 class="font-bold mb-16">Components</h1>
+    <p>
+      Components are one of the key building blocks of the design system. Each
+      component has been designed and coded to solve a specific UI problem, such
+      as presenting a list of options, enabling submission of a form, providing
+      feedback to the user, and so on. All of the components in have been
+      designed to work harmoniously together, as parts of a greater whole.
+    </p>
   </div>
 </template>
