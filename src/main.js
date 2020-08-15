43963a17235119b5418c23dbb1919c26bc2ca494
Raphi <raphael.antonietti@liip.ch>
2020-08-15 18:18:33 +0200

    small easteregg

--- src/main.js
+++ src/main.js
@@ -16,6 +16,20 @@ Vue.component("prism", Prism);
 Vue.component("svg-icon", SVGIcon);
 Vue.component("btn", Btn);
 
+// store the original tab title
+const origTitle = document.title;
+// function to change title when focusing on tab
+function setOldTitle() {
+  document.title = origTitle;
+}
+// function to change title when un-focusing on tab
+function setNewTitle() {
+  document.title = "Thank you for visiting!";
+}
+// bind functions to blur and focus events
+window.onblur = setNewTitle;
+window.onfocus = setOldTitle;
+
 new Vue({
   router,
   store,
