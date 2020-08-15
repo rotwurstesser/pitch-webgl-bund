b603af7e0245bcf91a85641a8f4092123e7a4309
Raphi <raphael.antonietti@liip.ch>
2020-08-15 15:42:02 +0200

    Add picto page

--- src/views/components/Pictograms.vue
+++ src/views/components/Pictograms.vue
@@ -1,24 +1,54 @@
 <template>
-  <div class="space-y-6 max-w-4xl icons">
-    <h1 class="font-bold mb-16">Icons</h1>
+  <div class="space-y-6 max-w-4xl">
+    <h1 class="font-bold mb-16">Pictograms</h1>
+    <info-box class="my-8">
+      <p>
+        The Pictos we picked on this page were exctracted from some Cantonal
+        flags, the choice is random and serves as an example.
+      </p>
+    </info-box>
     <p>
-      Icons are the bomb
+      Pictographs are used in writing and graphic systems in which the
+      characters are to a considerable extent pictorial in appearance. The
+      symbolic Language we picked represents the Diversity of Siwtzerland and is
+      therefore a Keyvisual to convey the Brand of the Swiss Confederation.
     </p>
-    <h2>Helpers</h2>
-    <div class="flex flex-wrap space-x-4">
-      <svg-icon class="text-red-600 fill-current" name="chevron-left" />
-      <svg-icon class="text-red-600 fill-current" name="loading" />
-      <svg-icon class="text-red-600 fill-current" name="close" />
-    </div>
-    <h2>Cantons</h2>
-    <div class="flex flex-wrap space-x-4">
-      <svg-icon name="bern" />
-      <svg-icon name="glarus" />
-      <svg-icon name="luzern" />
-      <svg-icon name="schwyz" />
-      <svg-icon name="zuerich" />
-      <svg-icon name="uri" />
-    </div>
+    <code-loader
+      figmalink="https://www.figma.com/file/tohWKnhnuXCVoG1o1br2MK/Bund-Design-System?node-id=430%3A0"
+      lang="html"
+    >
+      <svg-icon class="h-56  w-auto" name="cantons" />
+    </code-loader>
+    <code-loader
+      figmalink="https://www.figma.com/file/tohWKnhnuXCVoG1o1br2MK/Bund-Design-System?node-id=431%3A2"
+      lang="html"
+    >
+      <svg-icon class="h-56 w-auto" name="crosses" />
+    </code-loader>
+    <h2>Example Usage</h2>
+    <code-loader
+      figmalink="https://www.figma.com/file/tohWKnhnuXCVoG1o1br2MK/Bund-Design-System?node-id=433%3A0"
+      lang="html"
+    >
+      <div class="p-24 relative">
+        <div class="relative max-w-6xl mt-10">
+          <svg-icon
+            class="-translate-y-1/2 absolute h-24 right-0 top-0 transform translate-x-1/2 w-auto"
+            name="crosses"
+          />
+          <img src="@/assets/bund.png" class="" />
+        </div>
+      </div>
+      <div class="p-24 relative">
+        <div class="relative max-w-6xl mt-10">
+          <svg-icon
+            class="-translate-y-1/2 absolute h-24 right-0 top-0 transform translate-x-1/2 w-auto"
+            name="cantons"
+          />
+          <img src="@/assets/bund.png" class="" />
+        </div>
+      </div>
+    </code-loader>
   </div>
 </template>
 <style>
@@ -27,3 +57,7 @@
   width: 24px;
 }
 </style>
+<script>
+import InfoBox from "../../components/InfoBox.vue";
+export default { components: { InfoBox } };
+</script>
--- src/views/design/Icons.vue
+++ src/views/design/Icons.vue
@@ -10,18 +10,7 @@
       <svg-icon class="mx-2 stroke-2 inline-block" name="figma" /> icon
     </p>
     <br />
-    <h2>Swiss Pictograms</h2>
-    <p>
-      Sometimes it makes more Sense to use Pictograms instead of Icons in this
-      case we recommend using these:
-    </p>
-    <iframe
-      style="border: 1px solid rgba(0, 0, 0, 0.1);"
-      width="800"
-      height="450"
-      src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FtohWKnhnuXCVoG1o1br2MK%2FBund-Design-System%3Fnode-id%3D130%253A3984&chrome=DOCUMENTATION"
-      allowfullscreen
-    ></iframe>
+
     <h2>Action Icons</h2>
     <p>Action icons are used to signal and describe interaction.</p>
     <iframe
