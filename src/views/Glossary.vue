9ed4d093a9813ff7626fa17ef0360e8d20f60d4b
Raphi <raphael.antonietti@liip.ch>
2020-08-15 14:13:27 +0200

    apply correct rythm

--- src/views/Glossary.vue
+++ src/views/Glossary.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1 class="font-bold mb-16">Glossary</h1>
-    <h2 class="mb-12">
+    <h2 class="mb-6 mt-8">
       WEBGL
     </h2>
     <p class="mb-3">
@@ -10,7 +10,7 @@
       officiis sunt. Expedita dolore architecto quibusdam modi, voluptas
       obcaecati.
     </p>
-    <h2 class="mb-12">
+    <h2 class="mb-6 mt-8">
       Design System
     </h2>
     <p class="mb-3">
@@ -19,7 +19,7 @@
       officiis sunt. Expedita dolore architecto quibusdam modi, voluptas
       obcaecati.
     </p>
-    <h2 class="mb-12">
+    <h2 class="mb-6 mt-8">
       BEM
     </h2>
     <p class="mb-3">
@@ -28,7 +28,7 @@
       officiis sunt. Expedita dolore architecto quibusdam modi, voluptas
       obcaecati.
     </p>
-    <h2 class="mb-12">
+    <h2 class="mb-6 mt-8">
       Atom
     </h2>
     <p class="mb-3">
@@ -37,7 +37,7 @@
       officiis sunt. Expedita dolore architecto quibusdam modi, voluptas
       obcaecati.
     </p>
-    <h2 class="mb-12">
+    <h2 class="mb-6 mt-8">
       Molecule
     </h2>
     <p class="mb-3">
@@ -46,7 +46,7 @@
       officiis sunt. Expedita dolore architecto quibusdam modi, voluptas
       obcaecati.
     </p>
-    <h2 class="mb-12">
+    <h2 class="mb-6 mt-8">
       Component
     </h2>
     <p class="mb-3">
@@ -55,7 +55,7 @@
       officiis sunt. Expedita dolore architecto quibusdam modi, voluptas
       obcaecati.
     </p>
-    <h2 class="mb-12">
+    <h2 class="mb-6 mt-8">
       Page
     </h2>
     <p class="mb-3">
@@ -64,8 +64,8 @@
       officiis sunt. Expedita dolore architecto quibusdam modi, voluptas
       obcaecati.
     </p>
-    <h2 class="mb-12">
-      Composition?
+    <h2 class="mb-6 mt-8">
+      Page
     </h2>
     <p class="mb-3">
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit, provident
