ce3d9527f86385dcf20eea374d44641af35197a1
Raphi <raphael.antonietti@liip.ch>
2020-08-16 21:44:09 +0200

    swissness

--- src/views/components/GitVersion.vue
+++ src/views/components/GitVersion.vue
@@ -3,7 +3,7 @@
     <a class="flex space-x-4 no-underline" href="">
       <svg-icon name="github" /> <span>Go to Github</span>
     </a>
-    <span class="block">Version 0.16.6</span>
+    <span class="block">Version 0.12.91</span>
     <router-link
       to="/contribute"
       class="border-0 border-gray-400 block btn btn--tertiary btn--sm cursor-pointer inline-block"
