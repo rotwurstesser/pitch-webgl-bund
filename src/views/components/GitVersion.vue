c4257f8163c0c202d3783303d1f99a4c8d76ad8c
Sylvain Aerni <sylvain.aerni@gmail.com>
2020-08-16 21:19:05 +0200

    fix link colors

--- src/views/components/GitVersion.vue
+++ src/views/components/GitVersion.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="fixed github-footer text-center w-64">
-    <a class="flex justify-center space-x-4 no-underline" href="">
-      <svg-icon name="github" /> <span>Github</span>
+  <div class="fixed github-footer p-4 w-64">
+    <a class="flex space-x-4 no-underline" href="">
+      <svg-icon name="github" /> <span>Go to Github</span>
     </a>
     <span class="block">Version 0.16.6</span>
     <router-link
       to="/contribute"
-      class="border-0 border-gray-400 block btn btn--tertiary btn--xs cursor-pointer inline-block"
+      class="border-0 border-gray-400 block btn btn--tertiary btn--sm cursor-pointer inline-block"
     >
       Contribute
     </router-link>
