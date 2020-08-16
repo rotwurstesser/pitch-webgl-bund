2423e96322349647f799c1f4dcfe506ac477af5c
Raphi <raphael.antonietti@liip.ch>
2020-08-16 12:13:08 +0200

    Talk about Tailwind and design assets

--- src/views/Components.vue
+++ src/views/Components.vue
@@ -10,5 +10,16 @@
       Elements in the system will continually expanded upon. Use the navigation
       to your right to navigate through them.
     </p>
+    <h2>Technologies used</h2>
+    <p>
+      The Components are built with Tailwind and it's @apply function, this
+      allows to implement BEM-Name conventions rather easily. Every component
+      has a link to it's Figma file, allowing designers to start prototyping
+      without the need to copy all the elements by hand. The Styleguide also
+      offers the up to date Sketch Library.
+    </p>
+    <btn variant="link">Learn more about Tailwind</btn>
+    <btn variant="link" class="mb-12">Download Sketch Library</btn>
+    <btn variant="link" class="mb-12">Open Figma Library</btn>
   </div>
 </template>
