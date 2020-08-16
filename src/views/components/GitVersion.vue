8bc65b19cefd89c08a861fd3558cc30485dd2f5b
Sylvain Aerni <sylvain.aerni@gmail.com>
2020-08-16 21:54:32 +0200

    github box improvements

--- src/views/components/GitVersion.vue
+++ src/views/components/GitVersion.vue
@@ -1,16 +1,21 @@
 <template>
-  <div class="fixed github-footer p-4 w-64">
-    <a class="flex space-x-4 no-underline" href="">
-      <svg-icon name="github" /> <span>Go to Github</span>
-    </a>
-    <span class="block">Version 0.16.6</span>
-    <router-link
+  <div class="flex fixed github-footer w-64 bg-white border-t border-r border-gray-400">
+    <div class="border-r border-gray-400 p-4 w-1/2">
+      <a class="flex no-underline text-xs" href="">
+        <span>Github</span>
+      </a>
+      <span class="block text-xs">Version 0.12.91</span>
+    </div>
+    <div class="p-3 w-1/2">
+      <router-link
       to="/contribute"
-      class="border-0 border-gray-400 block btn btn--tertiary btn--sm cursor-pointer inline-block"
+      class="border-0 border-gray-400 block btn btn--tertiary btn--xs cursor-pointer inline-block"
     >
       Contribute
     </router-link>
+    </div>
   </div>
+
 </template>
 
 <script>
@@ -18,6 +23,6 @@ export default {};
 </script>
 <style>
 .github-footer {
-  bottom: 60px;
+  bottom: 38px;
 }
 </style>
